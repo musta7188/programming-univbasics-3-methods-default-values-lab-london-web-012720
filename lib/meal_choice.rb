d3a531820c7265f090e82e5cfac30d0edd8f9af3
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-22 12:20:58 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,15 +1,13 @@
 
-def meal_choice
-puts "What a nutritious meal!"
+
+
+def meal_choice(veg1, veg2, protein ="meat")
+  puts "What a nutritious meal!"
+
+choice =  "A plate of #{protein} with #{veg1} and #{veg2}."
+
+
+puts choice
+return choice
 
 end
-
-
-
-def meal_choice(veg1, veg2, protein = "meat")
-p "A plate of #{protein} with #{veg1} and #{veg2}."
-
-end
-meal_choice()
-
-meal_choice("broccoli", "macaroni")
